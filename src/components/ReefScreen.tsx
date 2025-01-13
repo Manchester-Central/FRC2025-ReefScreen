503b29d9ec5c01085ba4e9f2c44e737d69dca7e0
NinjaBuilderPro <NinjaBuilderPro@users.noreply.github.com>
2025-01-13 13:12:34 -0500

    Added hexagons

--- src/components/ReefScreen.tsx
+++ src/components/ReefScreen.tsx
@@ -1,10 +1,13 @@
-import ReefLevel from "./ReefLevel";
-import ReefPosition from "./ReefPosition";
+import Hexagon from "./Hexagon";
 
 const ReefScreen = () => {
     return (
         <div className="ReefScreen">
-            <ReefPosition/>
+            <div style={{position: "absolute", zIndex:-1, top: 250, right: 535}}><Hexagon size={150} color="gray"/></div>
+            <div style={{position: "absolute", zIndex:-2, top: 175, right: 450}}><Hexagon size={300} color="lightblue"/></div>
+            <div style={{position: "absolute", zIndex:-3, top: 100.4, right: 366}}><Hexagon size={450} color="blue"/></div>
+            <div style={{position: "absolute", zIndex:-4, top: 27, right: 278}}><Hexagon size={600} color="darkblue"/></div>
+            {/* <ReefPosition/>
             <div className="BarrierLine" style={{
                 position: "absolute",
                 height: "675px",
@@ -13,7 +16,7 @@ const ReefScreen = () => {
                 top: "37.5px",
                 left: "480px"
             }}/>
-            <ReefLevel/>
+            <ReefLevel/> */}
         </div>
     )
 }
